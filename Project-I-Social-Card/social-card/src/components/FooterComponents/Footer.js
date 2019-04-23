410e0bb5ebc3e644ba0eb739eeba79f0c11e50a5
Andy Bettisworth <andy@accreu.com>
2019-04-23 16:10:55 -0500

    Define click handler for Footer component

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -13,24 +13,48 @@ const React = require('react')
 require('./Footer.scss')
 
 /**
+ * Define click handler
+ */
+
+const clickHandler = event => {
+  switch(event.currentTarget.dataset.action) {
+    case 'comment':
+      event.currentTarget.style.backgroundColor = "blue"
+      break
+    case 'sync':
+      event.currentTarget.style.backgroundColor = "green"
+      break
+    case 'like':
+      event.currentTarget.style.backgroundColor = "orange"
+      break
+    case 'mail':
+      event.currentTarget.style.backgroundColor = "red"
+      break
+    default:
+      // Do nothing
+      break
+  }
+}
+
+/**
  * Define component
  */
 
 const Footer = () => {
   return (
     <div className="jsx-Footer">
-      <div class="jsx-Footer__icon">
+      <button onClick={clickHandler} className="jsx-Footer__icon" data-action="comment">
         <i className="far fa-comment fa-flip-horizontal"></i>
-      </div>
-      <div class="jsx-Footer__icon">
-        <i className="fas fa-sync"></i> <span class="jsx-Footer__icon--padding-left">6</span>
-      </div>
-      <div class="jsx-Footer__icon">
-        <i className="far fa-heart"></i> <span class="jsx-Footer__icon--padding-left">4</span>
-      </div>
-      <div class="jsx-Footer__icon">
+      </button>
+      <button onClick={clickHandler} className="jsx-Footer__icon" data-action="sync">
+        <i className="fas fa-sync"></i> <span className="jsx-Footer__icon--padding-left">6</span>
+      </button>
+      <button onClick={clickHandler} className="jsx-Footer__icon" data-action="like">
+        <i className="far fa-heart"></i> <span className="jsx-Footer__icon--padding-left">4</span>
+      </button>
+      <button onClick={clickHandler} className="jsx-Footer__icon" data-action="mail">
         <i className="far fa-envelope"></i>
-      </div>
+      </button>
     </div>
   )
 }
